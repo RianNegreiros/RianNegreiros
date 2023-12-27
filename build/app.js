521a4a57cf18eddab025d4750840b22e1fb2e45c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-12-27 16:01:06 +0000

    docs: auto-update README.md

---
+++ build/app.js
@@ -0,0 +1,92 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const md = require("markdown-it")({
+    html: true, // Enable HTML tags in source
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    linkify: true // Autoconvert URL-like text to links
+});
+const fetchRssData_1 = require("./fetchRssData");
+const blogFeedUrl = "https://personalwebsitebackend.azurewebsites.net/api/rss";
+const githubUsername = "RianNegreiros";
+const websiteUrl = "https://riannegreiros.dev";
+const blogUrl = "https://riannegreiros.dev/posts";
+const linkedinUrl = "https://linkedin.com/in/riannegreiros";
+function generateMarkdown() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
+        const hashnodeBadge = `[![Blog Badge](https://img.shields.io/badge/-Blog-3B7EBF?style=for-the-badge&logo=Hashnode&logoColor=white)](${blogUrl})`;
+        const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
+        const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
+        const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+        const markdownText = `<div align="center">\n
+
+  ${websiteBadge} ${hashnodeBadge} ${linkedinBadge} \n
+
+  ---\n
+
+  Desenvolvedor back-end do Rio de Janeiro. Iniciei minha jornada na adolescência, configurando servidores para o jogo DayZ usando Batchfile. Atualmente, estou embarcando na jornada do desenvolvimento, ansioso para aprender e contribuir para a comunidade.
+
+  ---\n
+
+  ${githubStatsCardDark}\n
+  ${githubStatsCardLight}\n
+
+  </div>\n
+
+  ---\n
+
+  ## Destaques 
+
+  <details>\n
+  <summary>Blogposts recentes</summary>\n
+  <br />
+  ${yield (0, fetchRssData_1.fetchRssData)(blogFeedUrl)}\n
+  </details>\n
+
+  ---\n
+
+  <div align="center">\n
+    <a href="https://riannegreiros.dev" target="_blank" rel="noopener noreferrer"><img src="https://riannegreiros.dev/favicon.ico" width="30" /></a>\n
+  </div>`;
+        const result = md.render(markdownText);
+        fs.writeFile("README.md", result, (error) => {
+            if (error)
+                throw new Error(`Something went wrong: ${error}.`);
+            console.log(`✅ README.md file was succesfully generated.`);
+        });
+    });
+}
+generateMarkdown();
