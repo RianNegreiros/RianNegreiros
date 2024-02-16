4905cc98522aed66707a11ca2754dfb572f0631e
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-16 18:36:25 +0000

    docs: auto-update README.md

--- build/app.js
+++ build/app.js
@@ -42,12 +42,10 @@ const fetchRssData_1 = require("./fetchRssData");
 const blogFeedUrl = "https://personalwebsitebackend.azurewebsites.net/api/posts/rss";
 const githubUsername = "RianNegreiros";
 const websiteUrl = "https://riannegreiros.dev";
-const blogUrl = "https://riannegreiros.dev/posts";
 const linkedinUrl = "https://linkedin.com/in/riannegreiros";
 function generateMarkdown() {
     return __awaiter(this, void 0, void 0, function* () {
         const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
-        const hashnodeBadge = `[![Blog Badge](https://img.shields.io/badge/-Blog-3B7EBF?style=for-the-badge&logo=Hashnode&logoColor=white)](${blogUrl})`;
         const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
         const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats-three-iota-97.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
         const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats-three-iota-97.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
@@ -56,7 +54,7 @@ function generateMarkdown() {
         const markdownText = `
 <div align="center">
 
-  ${websiteBadge} ${hashnodeBadge} ${linkedinBadge}
+  ${websiteBadge} ${linkedinBadge}
 
   ---
 
