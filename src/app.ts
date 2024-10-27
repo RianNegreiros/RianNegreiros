826decc6a713ff8ba78191d3f3c387186e088685
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-10-27 19:56:22 -0300

    style: run prettier

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
-import fs from 'fs';
-import MarkdownIt from 'markdown-it';
-import { fetchRssData } from './fetchRssData';
+import fs from 'fs'
+import MarkdownIt from 'markdown-it'
+import { fetchRssData } from './fetchRssData'
 
 const md = new MarkdownIt({
   html: true,
   breaks: true,
   linkify: true,
-});
+})
 
-const blogFeedUrl = 'https://riannegreiros.dev/api/rss';
-const githubUsername = 'RianNegreiros';
-const websiteUrl = 'https://www.riannegreiros.dev';
-const linkedinUrl = 'https://linkedin.com/in/riannegreiros';
+const blogFeedUrl = 'https://riannegreiros.dev/api/rss'
+const githubUsername = 'RianNegreiros'
+const websiteUrl = 'https://www.riannegreiros.dev'
+const linkedinUrl = 'https://linkedin.com/in/riannegreiros'
 
 async function generateMarkdown() {
   try {
-    const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
-    const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
-    const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats-three-iota-97.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
-    const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats-three-iota-97.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
-    const githubLanguagesStatsCardDark = `[![GitHub-Languages-Stats-Card-Dark](https://github-readme-stats.vercel.app/api/top-langs?username=${githubUsername}&layout=compact&hide_border=true&card_width=600&hide=typescript&custom_title=GitHub%20Languages%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
-    const githubLanguagesStatsCardLight = `[![GitHub-Languages-Stats-Card-Light](https://github-readme-stats.vercel.app/api/top-langs?username=${githubUsername}&layout=compact&hide_border=true&card_width=600&hide=typescript&custom_title=GitHub%20Languages%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+    const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`
+    const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`
+    const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats-three-iota-97.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`
+    const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats-three-iota-97.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`
+    const githubLanguagesStatsCardDark = `[![GitHub-Languages-Stats-Card-Dark](https://github-readme-stats.vercel.app/api/top-langs?username=${githubUsername}&layout=compact&hide_border=true&card_width=600&hide=typescript&custom_title=GitHub%20Languages%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`
+    const githubLanguagesStatsCardLight = `[![GitHub-Languages-Stats-Card-Light](https://github-readme-stats.vercel.app/api/top-langs?username=${githubUsername}&layout=compact&hide_border=true&card_width=600&hide=typescript&custom_title=GitHub%20Languages%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`
 
-    const recentPostsMarkdown = await fetchRssData(blogFeedUrl);
+    const recentPostsMarkdown = await fetchRssData(blogFeedUrl)
 
     const markdownText = `
 <div align="center">
@@ -51,17 +51,17 @@ async function generateMarkdown() {
 ## Posts recentes
 
 ${recentPostsMarkdown}
-`;
+`
 
-    const renderedMarkdown = md.render(markdownText);
+    const renderedMarkdown = md.render(markdownText)
 
     fs.writeFile('README.md', renderedMarkdown, (error) => {
-      if (error) throw new Error(`Something went wrong: ${error}.`);
-      console.log('✅ README.md file was successfully generated.');
-    });
+      if (error) throw new Error(`Something went wrong: ${error}.`)
+      console.log('✅ README.md file was successfully generated.')
+    })
   } catch (error) {
-    console.error(`Error generating markdown: ${error}`);
+    console.error(`Error generating markdown: ${error}`)
   }
 }
 
-generateMarkdown();
+generateMarkdown()
