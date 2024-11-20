08c680c9e986a99c0c1d97c679845d8e38c231e8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-11-20 10:10:05 -0300

    style: update and reconfig eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,9 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   {
     rules: {
